98274cbdcd78ff74a52f66b64217bc571c31398d
Pendo Vestine <144009226+vpendo@users.noreply.github.com>
2024-06-09 22:00:27 +0200

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists
 
-env.hosts = ['54.242.133.62', '34.233.128.212']
+env.hosts = ['54.205.77.135', '3.80.82.79']
 env.user = "ubuntu"
 env.key = "~/.ssh/id_rsa"
 
